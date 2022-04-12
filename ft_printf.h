96671317c00cb89a4b2e0de0f4ec2464bfd582df
Denny Cerritos portillo <dcerrito@c3r13s5.42madrid.com>
2022-04-12 03:47:01 +0200

    chores: update header definition

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 04:30:17 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/04/11 23:54:06 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/04/12 03:23:42 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 # include "libft/libft.h"
 
 int	ft_printf(const char *format, ...);
-int	ft_putnbr_base(long n, char *base);
+int	ft_putnbr_base(long n, char *base, int omit_print_neg);
 
 #endif
